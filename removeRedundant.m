5affeef98ca856acd9e35356b9c7b1c4e02a4f10
Katarzyna Figas <kat.figas@gmail.com>
2020-06-25 00:50:19 +0200

    remove redundant removeRedundant

--- removeRedundant.m
+++
@@ -1,8 +0,0 @@
-if not(exist('set', 'var'))
-    dataSetPath = 'D:\nauka\nauka\infa\ssn projekt\ssn_projekt_data_set\batched\full_set.mat'
-    dataSetStruct = load(dataSetPath)
-    set = dataSetStruct.set;
-end
-
-
-set.category(set.category == 'THE WORLDPOST') = 'WORLDPOST'
